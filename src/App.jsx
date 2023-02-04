3df59be9ae38d6a21ee059b79a20232ab068a5c3
Nikhil <nikhilsimontoppo@gmail.com>
2023-02-04 23:54:49 +0530

    Added icons and some description. Corrected color theme

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,10 @@ function App() {
   }, 800);
 
   return (
-    <div>
-      <div id="cover" class="flex flex-col border gap-10 items-center">
+    <div class="text-white bg-gradient-to-r from-[#0f0c29] to-[#302b63]">
+      <div id="cover" class="flex flex-col  gap-10 items-center">
         <h1 class="text-center text-6xl mx-auto mt-[36vh]">
-          Hi! I am <span class="text-blue-400">Simon</span> <br /> I am a
+          Hi! I'm <span class="text-blue-400">Simon</span> <br /> I am a
           <span class="text-blue-500"> Frontend </span> Engineer
           <br /> and <br /> User Experience
           <span class="text-blue-600"> Professional</span>
@@ -31,20 +31,107 @@ function App() {
         <h1 class="text-xl font-semibold text-blue-500">Most Developed</h1>
         <h1 class="text-4xl font-bold">Skills</h1>
         <p class="mt-[5vh] text-xl text-center">
-          Some of the few niche's at which I've gained adequate proficiency.{" "}
+          Some of the few niches at which I've gained adequate proficiency.{" "}
           <br />
           Learning is endless ofcourse, but I'm quite confident in the following
           stuff.
         </p>
-        <div class="flex gap-5 mt-[5vh]">
-          <span>React/Preact</span>
-          <span>Deno/Fresh</span>
-          <span>Javscript/Typescript</span>
+        <div class="flex gap-[16vh] mt-[5vh]">
+          <span class="flex flex-col items-center">
+            <img
+              src="preact.svg"
+              class=" hover:w-[180px] hover:transition-all transition-all w-[140px]"
+              alt=""
+            />
+            React/Preact
+          </span>
+          <span class="flex flex-col items-center">
+            <img
+              src="deno.svg"
+              class=" hover:w-[180px] hover:transition-all transition-all w-[140px]"
+              alt=""
+            />
+            Deno/Fresh
+          </span>
+          <span class="flex flex-col items-center">
+            <img
+              src="typescript.svg"
+              class=" hover:w-[180px] hover:transition-all transition-all w-[140px]"
+              alt=""
+            />
+            Javscript/Typescript
+          </span>
         </div>
-        <div className="flex gap-5 mt-[5vh]">
-          <span>SolidJS</span>
-          <span>Figma</span>
+        <div className="flex gap-[6vh] mt-[5vh]">
+          <span class="flex flex-col items-center">
+            <img
+              src="solidjs.svg"
+              class=" hover:w-[180px] hover:transition-all transition-all w-[140px]"
+              alt=""
+            />
+            SolidJS
+          </span>
+          <span class="flex flex-col items-center">
+            <img
+              src="figma.svg"
+              class=" hover:w-[180px] hover:transition-all transition-all w-[140px]"
+              alt=""
+            />
+            Figma
+          </span>
         </div>
+      </div>
+
+      <div
+        id="project"
+        class="flex flex-col mt-[15vh]  mx-[10%] items-center gap-[3vh] justify-center"
+      >
+        <h1 class="text-xl font-semibold text-blue-500">Projects</h1>
+        <h1 class="text-4xl  font-bold">Pokédex</h1>
+        <img src="backpoke.jpg" class=" rounded-md w-[800px]" alt="" />
+        <span class="flex gap-10">
+          <a
+            href=""
+            class="bg-pink-300 text-black hover:bg-pink-500 transition-all px-5 py-1 rounded-md"
+          >
+            Live Site
+          </a>
+          <a
+            href=""
+            class="bg-pink-300 text-black hover:bg-pink-500 transition-all px-5 py-1 rounded-md"
+          >
+            View Repo
+          </a>
+        </span>
+        <span class=" text-gray-300 text-xl">
+          <p>
+            This was one of my favourite projects since I'm hardcore Pokéfan!
+            It's a Pokédex (Pokémon Library) which contains info about all 746
+            Pokémons! Includes Pokemon's description and move's types.
+          </p>
+          <p class="mt-5">
+            It runs on the recently released JS-runtime, Deno. Built on top of
+            the Fullstack framework for Deno, Fresh. Styled using TailwindCSS
+            and deployed though Netlify for reliable performance.
+          </p>
+        </span>
+
+        <h1 class="text-4xl  font-bold">Fashion Ecommerce</h1>
+        <img src="backpoke.jpg" class=" rounded-md w-[800px]" alt="" />
+        <span class="flex gap-10">
+          <a
+            href=""
+            class="bg-pink-300 text-black hover:bg-pink-500 transition-all px-5 py-1 rounded-md"
+          >
+            Live Site
+          </a>
+          <a
+            href=""
+            class="bg-pink-300 text-black hover:bg-pink-500 transition-all px-5 py-1 rounded-md"
+          >
+            View Repo
+          </a>
+        </span>
       </div>
     </div>
   );
